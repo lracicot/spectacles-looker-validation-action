af5705e06ee8ec1b8268278608a0dd6cb8863d77
Louis Racicot <louis.racicot@alayacare.com>
2022-06-20 20:44:46 -0400

    add validations

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-spectacles $INPUT_VALIDATOR  --project $INPUT_LOOKML_PROJECT --branch $INPUT_BRANCH
+spectacles $INPUT_VALIDATOR  --project $INPUT_LOOKML_PROJECT --branch $INPUT_BRANCH --incremental --api-version 4.0
